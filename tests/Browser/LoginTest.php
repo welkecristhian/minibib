fdddfe284649b4e9c2fe8b4e66189d509c16aaa7
welke <welkecristhiancarneirolima@usp.br>
2026-08-25 14:24:46 -0300

    Adicionando LoginTest

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     */
+    public function test_example(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->clickLink('Entrar')
+                ->waitForText('Usuário')
+                ->type('#loginUsuario', '111111')
+                ->press('Login')
+                ->assertSee('Sair');
+        });
+    }
+}
